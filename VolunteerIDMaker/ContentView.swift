a3c128d6a181d55f168d5f66b769ffe0cf9c5b84
Irvin <irvin999rafael6@csu.fullerton.edu>
2022-06-13 13:16:33 -0700

    Module 9, Model 1-3 on June 13 changes

--- VolunteerIDMaker/ContentView.swift
+++ VolunteerIDMaker/ContentView.swift
@@ -10,37 +10,55 @@ import SwiftUI
 struct ContentView: View {
     @State var name: String = ""
     @State var address: String = ""
+    @State var age: String = ""
     var body: some View {
         // TODO: Enclose this entire VStack with GeometryReader in Model 3
-        VStack {
-            // TODO: Add frame method call to this VStack in Model 3
+        GeometryReader { geometry in
             VStack {
-                Text("SafeWalk Volunteer")
-                HStack {
-                    Spacer()
-                    Text("Name: ")
-                    TextField("Name", text: $name)
-                    Spacer()
+                // TODO: Add frame method call to this VStack in
+                // Model 3
+                VStack {
+                    Text("SafeWalk Volunteer")
+                    HStack {
+                        Spacer()
+                        Text("Name: ")
+                        TextField("Name", text: $name)
+                        Spacer()
+                    }
+                    HStack {
+                        Spacer()
+                        Text("Address: ")
+                        TextField("Address", text: $address)
+                        Spacer()
+                    }
+                    HStack {
+                        Spacer()
+                        Text("Age: ")
+                        TextField("Age", text: $age)
+                        Spacer()
+                    }
+                }.frame(height: geometry.size.height / 3)
+                Spacer()
+                /* TODO: Replace the entire Text VStack below with sub view provided in Model 2
+                 */
+                VStack {
+                    Information(name: $name, address: $address, age: $age)
                 }
-                HStack {
-                    Spacer()
-                    Text("Address: ")
-                    TextField("Address", text: $address)
-                    Spacer()
-                }
-            }
-            Spacer()
-            /* TODO: Replace the entire Text VStack below with sub view provided in Model 2
-             */
-            VStack {
-                Text(name)
-                Text(address)
-            }
         }
     }
 }
 
 // TODO: Add Information structure provided in Model 2
+struct Information: View {
+    @Binding var name: String
+    @Binding var address: String
+    @Binding var age: String
+    var body: some View {
+        Text(name)
+        Text(address)
+        Text(age)
+    }
+    }
 
 // TODO: Add custom modifier below then use it to customizes your views for Model 5
 
@@ -48,4 +66,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
 }
